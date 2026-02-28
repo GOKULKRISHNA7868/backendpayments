52c53c86b3f4d78d2e6c0884fd9f964891cfe8d9
Gokul Krishna <145557194+GOKULKRISHNA7868@users.noreply.github.com>
2026-02-28 18:14:47 +0530

    Refactor IV handling in encryption and decryption
    Replace hardcoded IV with a zeroed buffer for encryption and decryption.

--- utils/crypto.js
+++ utils/crypto.js
@@ -1,15 +1,8 @@
-const crypto = require("crypto");
-
 const algorithm = "aes-128-cbc";
 
-// CCAvenue standard IV
-const iv = Buffer.from([
-  0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c,
-  0x0d, 0x0e, 0x0f,
-]);
-
 exports.encrypt = (plainText, workingKey) => {
-  const key = crypto.createHash("md5").update(workingKey).digest(); // 16 bytes
+  const key = crypto.createHash("md5").update(workingKey).digest();
+  const iv = Buffer.alloc(16, "\0");
   const cipher = crypto.createCipheriv(algorithm, key, iv);
   let encrypted = cipher.update(plainText, "utf8", "hex");
   encrypted += cipher.final("hex");
@@ -18,6 +11,7 @@ exports.encrypt = (plainText, workingKey) => {
 
 exports.decrypt = (encText, workingKey) => {
   const key = crypto.createHash("md5").update(workingKey).digest();
+  const iv = Buffer.alloc(16, "\0");
   const decipher = crypto.createDecipheriv(algorithm, key, iv);
   let decrypted = decipher.update(encText, "hex", "utf8");
   decrypted += decipher.final("utf8");
